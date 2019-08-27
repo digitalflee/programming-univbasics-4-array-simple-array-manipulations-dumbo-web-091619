40645910591617d340cf8c8ed442c435fd3f8a63
Keenan Alves <keenan.alves@gmail.com>
2019-08-27 16:50:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,2 +1,11 @@
-def using_push(array, string)
-  array.push(string)
+def using_push(Array, String)
+  array.push(String)
+end
+
+def using_unshift(Array, String)
+  array.unshift(String)
+end
+
+def using_pop(Array)
+  Array.pop 
+end
